fa1e32864592e8bfebd8bf0211b9ed280101c84f
woojin <jwjley@naver.com>
2023-10-18 20:07:55 +0900

    feat : 8주차 실습 파일 (member 가입)

--- shop/src/main/java/inhatc/spring/shop/controller/MemberController.java
+++ shop/src/main/java/inhatc/spring/shop/controller/MemberController.java
@@ -1,19 +1,36 @@
 package inhatc.spring.shop.controller;
 
+import inhatc.spring.shop.Entity.Member;
+import inhatc.spring.shop.dto.MemberFormDto;
 import inhatc.spring.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/member/new")
-    public String memberForm() {
-
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+    }
+
+    @PostMapping("/member/new")
+    public String insertMember(MemberFormDto memberFormDto) {
+        log.info("====================> memberFormDto : " + memberFormDto);
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+        return "redirect:/";
     }
 }
